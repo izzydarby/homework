1bf68bc6cde30dbc62355537990ee8aad3c1786a
izzydarby <isabelleldarby@gmail.com>
2015-10-04 20:26:32 -0400

    day 6 JS screenshots added

---
+++ day9/starter_code/js/style.js
@@ -0,0 +1,33 @@
+//user inputs keyword
+
+var city = val().toLowerCase();
+
+// get the string value of user's input
+$( "submit-btn" ).click(function() {
+  var text = $( this ).text();
+  $( "input" ).val( text );
+});
+
+//return image based off of input
+	if(city = "New York") {
+		console.log("New York")
+		background-image src
+	}
+	else if { 
+		(city = "Sydney")
+		console.log("Sydney");
+	}
+	else if { 
+		(city = "Los Angeles")
+		console.log("Los Angeles");
+	}
+	else if { 
+		(city = "San Francisco")
+		console.log("San Francisco");
+	}
+	else if { 
+		(city = "Austin")
+		console.log("Austin");
+	}
+
+};
