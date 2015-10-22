abeb892ca9969c7cbffb028d06319ad7dff8105f
izzydarby <isabelleldarby@gmail.com>
2015-10-21 21:43:07 -0400

    final project jQuery

--- final project/js/style.js
+++ final project/js/style.js
@@ -7,10 +7,37 @@ $('.mobile-menu').click(function(){
 
 });
 
+//1. Hide Button
+
+//2. on click of any of the icons, remove the hide class
+
+//3. on click of an icon show search
+
+$('.activate-search').click(function(e) {
+	var icon = $(this).attr('id');
+	
+	$('#search-box').removeClass('hide');
+
+	if (icon === "eggs") {
+		//switch the default text in the input
+		//update the options in the dropdown
+		$('.button-question').attr('placeholder',"How many?");
+		$('#select-measurement').addClass('hide');
+	} else {
+		$('.button-question').attr('placeholder',"How much?")
+		$('#select-measurement').removeClass('hide');
+	}
+});
+
+//4. 
+
 
 //get the value of the input with jQuery. target the input id .val
 
-	$('#form').submit(function(event){
+	var howMuch = $("input[name='howMuch']").val();
+
+
+	$('#form').validate(function(event){
 		event.preventDefault();
 
 		var userAnswer = $('input').val().toLowerCase(); 
